df47d62c76286c31e97d4da1af03878b64a793bb
aminhl <amin.hlel@esprit.tn>
2022-01-30 17:42:07 +0100

    ajout de l'entite image, relier l'entite image avec l'entite Ad, enrichir le script textures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,6 +4,7 @@ namespace App\DataFixtures;
 
 use App\Entity\Ad;
 use Faker\Factory;
+use App\Entity\Image;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 
@@ -26,6 +27,14 @@ class AppFixtures extends Fixture
                ->setContent($content)
                ->setPrice(mt_rand(30,200))
                ->setRooms(mt_rand(1,5));   
+            for($j=1; $j<= mt_rand(2,5);++$j){
+                $image = new Image();
+                $image->setUrl($faker->imageUrl())
+                      ->setCaption($faker->sentence())
+                      ->setAd($ad);
+                $manager->persist($image);
+            }
+
             $manager->persist($ad);
         }
         $manager->flush();
