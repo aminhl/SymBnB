cb2740b826cb2032dc5b89440aaae0e6ea1e234d
aminhl <amin.hlel@esprit.tn>
2022-01-30 10:51:24 +0100

    initial-commit, installation et page d'accueil

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+
+class HomeController extends Controller{
+    /**
+     * @Route("/",name="homepage")
+     *
+     * @return void
+     */
+    public function home(){
+        return $this->render("home.html.twig");
+    }
+
+}
+
+
+
+?>
